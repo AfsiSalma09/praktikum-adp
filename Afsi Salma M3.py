368639f80f62aa8742a3ad29780935e1614e1aa5
AfsiSalma09 <afsisalma425@gmail.com>
2025-03-18 21:46:45 +0700

    Add files via upload

---
+++ Afsi Salma M3.py
@@ -0,0 +1,12 @@
+M = int(input("Masukkan modal awal investasi anda : "))
+r = int(input("Suku bunga tahunan (%) : "))
+T = int(input("Target ivestasi yang ingin anda capai : "))
+tahun = 0
+M < T = True
+while M < T :
+    bunga = M*(r/100)
+    M_baru = M+bunga
+    T = tahun+1
+    print(f"Tahun ke-{T} : Rp.{M_baru}")
+    M < T = False
+    
