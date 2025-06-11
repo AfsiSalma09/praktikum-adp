4ee39a2059cedbae8c10279013daed4ef1c40fd9
AfsiSalma09 <afsisalma425@gmail.com>
2025-06-11 09:40:48 +0700

    Add files via upload

---
+++ logo adidas.py
@@ -0,0 +1,23 @@
+from pyfiglet import figlet_format
+from termcolor import colored, cprint
+import os
+os.system('cls')
+print()
+cprint("                    "+colored(' ','white','on_white'))
+cprint("                   "+colored('   ','white','on_white'))
+cprint("   "+colored('  ','white','on_white')+'             '+colored('     ','white','on_white')+'             '+colored('  ','white','on_white'))
+cprint("  "+colored('     ','white','on_white')+'          '+colored('       ','white','on_white')+'          '+colored('     ','white','on_white'))
+cprint(" "+colored('        ','white','on_white')+'       '+colored('         ','white','on_white')+'       '+colored('        ','white','on_white'))
+cprint("  "+colored('         ','white','on_white')+'    '+colored('           ','white','on_white')+'    '+colored('         ','white','on_white'))
+cprint("   "+colored('         ','white','on_white')+'  '+colored('             ','white','on_white')+'  '+colored('         ','white','on_white'))
+print()
+cprint('     '+colored('                              ','white','on_white'))
+print()
+cprint('        '+colored('                        ','white','on_white'))
+print()
+cprint('           '+ colored('     ','white','on_white')+' '+colored('      ','white','on_white')+' ' +colored('     ','white','on_white'))
+cprint('              '+ colored('  ','white','on_white')+'   '+colored('  ','white','on_white')+'   ' +colored('  ','white','on_white'))
+
+print()
+teks = figlet_format("ADIDAS", font="standard")
+print(teks)
---
+++ logo pertamina.py
@@ -0,0 +1,22 @@
+from pyfiglet import figlet_format
+from termcolor import colored, cprint
+import os
+os.system("cls")
+print()
+cprint('                           ' + colored("          ",'red','on_red'))
+cprint('                            ' + colored("          ",'red','on_red'))
+cprint('                             ' + colored("          ",'red','on_red'))
+cprint('                              ' + colored("          ",'red','on_red'))  
+print()
+cprint('                  ' + colored("          ",'blue','on_blue') +'  ' + colored("          ",'green','on_green'))
+cprint('                 ' + colored("          ",'blue','on_blue') +'  ' + colored("          ",'green','on_green'))
+cprint('                ' + colored("          ",'blue','on_blue') +'  ' + colored("          ",'green','on_green'))
+cprint('               ' + colored("          ",'blue','on_blue') +'  ' + colored("          ",'green','on_green'))
+cprint('              ' + colored("          ",'blue','on_blue'))
+cprint('             ' + colored("          ",'blue','on_blue'))
+cprint('            ' + colored("          ",'blue','on_blue'))
+cprint('           ' + colored("          ",'blue','on_blue'))
+print()
+
+teks = figlet_format("PERTAMINA", font="standard")
+print(teks)
